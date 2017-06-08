6772834d7e198eab762c02f3557fad9eb2bcba74
code-enginner <sina.us.2019@gmail.com>
2017-06-08 11:31:30 +0430

    This file contains all pages

---
+++ pages/filmInsert.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="data">
+            <ul>
+                <li><label for="filmName"><input type="text"></label></li>
+                <li><label for="rank"><input type="number"></label></li>
+                <li><label for="genre"><input type="text"></label></li>
+                <li><label for="awards"><input type="text"></label></li>
+                <li><label for="releaseDate"><input type="text"></label></li>
+                <li><label for="DVDNumber"><input type="text"></label></li>
+                <li><label for="productOf"><input type="text"></label></li>
+                <li><label for=""></label><input type="text"></li>
+                <li><label for=""></label><input type="text"></li>
+                <li><textarea name="" id="" cols="30" rows="10"></textarea></li>
+                <li><textarea name="" id="" cols="30" rows="10"></textarea></li>
+            </ul>
+        </div>
+        <div class="prevCover">
+            <img src="" alt="cover" title="cover">
+        </div>
+    </div>
+</div>
+
+<?php
---
+++ pages/login.php
@@ -0,0 +1,30 @@
+<div class="wrapAll">
+    <div class="loginContent">
+        <div class="wrapper1 col-lg-6">
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">Login </span>
+                <input type="text" id="username" class="form-control" placeholder="Enter Username Or Email" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon2">Pass </span>
+                <input type="text" id="password" class="form-control" placeholder="Enter Password" aria-describedby="basic-addon2">
+            </div>
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon2">Security code</span>
+                <input type="text" id="secCode" class="form-control" placeholder="Enter Security Code" aria-describedby="basic-addon2">
+            </div>
+            <button id="btnLogin">Login</button>
+        </div>
+
+        <div class="wrapper2 col-lg-6">
+            <div class="captcha">sina</div>
+        </div>
+        <div class="clearFix"></div>
+        <div class="wrapper3 col-lg-12">
+            sina
+        </div>
+    </div>
+</div>
+<div id="output"></div>
+
+<?php
